37b500ceaa701e9e0319d892c57fd87bcf825ca0
Shubham Rooter   -  Shubham Tiwari <94091556+shubham-rooter@users.noreply.github.com>
2022-12-13 14:55:18 +0530

    Add files via upload

---
+++ easyRecon.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+BLUE='\033[94m'
+RED='\033[91m'
+GREEN='\033[92m'
+ORANGE='\033[93m'
+RESET='\e[0m'
+echo -e "
+░██████╗██╗░░██╗██╗░░░██╗██████╗░██╗░░██╗░█████╗░███╗░░░███╗  ██████╗░░█████╗░░█████╗░████████╗███████╗██████╗░
+██╔════╝██║░░██║██║░░░██║██╔══██╗██║░░██║██╔══██╗████╗░████║  ██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██╔══██╗
+╚█████╗░███████║██║░░░██║██████╦╝███████║███████║██╔████╔██║  ██████╔╝██║░░██║██║░░██║░░░██║░░░█████╗░░██████╔╝
+░╚═══██╗██╔══██║██║░░░██║██╔══██╗██╔══██║██╔══██║██║╚██╔╝██║  ██╔══██╗██║░░██║██║░░██║░░░██║░░░██╔══╝░░██╔══██╗
+██████╔╝██║░░██║╚██████╔╝██████╦╝██║░░██║██║░░██║██║░╚═╝░██║  ██║░░██║╚█████╔╝╚█████╔╝░░░██║░░░███████╗██║░░██║
+╚═════╝░╚═╝░░╚═╝░╚═════╝░╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░░░░╚═╝  ╚═╝░░╚═╝░╚════╝░░╚════╝░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝ 
+                    "$GREEN [+] by Shubham Rooter"
+ "$GREEN [+] https://github.com/shubham-rooter"
+ "$GREEN [+] Recon Tool$RESET"
+"
+domain=$1
+if [[ -z $domain ]]; then
+	echo -e "$GREEN Usage: ./easyRecon.sh <domain.com>"
+	exit 1
+fi
+echo -e "\e[91m[-] EasyRecon will gather the following information for you\e[0m"
+echo -e "\e[34m[+]\e[0mAll existing domains of $domain"
+echo -e "\e[34m[+]\e[0mAll Live domains of $domain"
+echo -e "\e[34m[+]\e[0mAll URLs/Links of $domain"
+echo -e "\e[34m[+]\e[0mAll JS files of $domain"
+echo "	"
+echo -e "\e[5m	Installing / Updating Tools so take some time. \e[0m"
+echo "	"
+echo "	"
+
+
+sudo apt install httprobe -y > /dev/null 2>&1
+sudo apt install getallurls  > /dev/null 2>&1``````````````````````````````````````````````````````````````````````
+sudo go install github.com/tomnomnom/waybackurls@latest  > /dev/nul 2>&1
+sudo go install github.com/sensepost/gowitness@latest > /dev/nul 2>&1
+
+mkdir $domain
+
+echo -e "\e[93mEnumerating subdomains with SubFinder...\e[0m"
+subfinder -d $domain -silent > $domain/all-domains.txt
+
+echo $domain >> $domain/all-domains.txt
+echo -e "[- Task Done -] \e[92mAll domains of $domain are saved in all-domains.txt\e[0m"
+
+sleep 1 # wait for 1 second
+echo "	"
+echo -e "\e[93mChecking for LIVE domains, this may take some time.\e[0m"
+
+cat $domain/all-domains.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ":443" > $domain/live-domains.txt
+
+echo -e "[- Task done -] \e[92mResults are saved in live-domains.txt\e[0m"
+
+echo "	"
+echo -e "\e[93mFetching all URLs/links of $domain, this may take some time..\e[0m"
+cat $domain/live-domains.txt | waybackurls | sort -u > $domain/urls.txt
+echo -e "[- Task Done -] \e[92mAll urls/links of $domain are saved in urls.txt\e[0m"
+
+echo "	"
+echo -e "\e[93mEnumerating all JS files from $domain, this may take some time\e[0m"
+cat $domain/urls.txt | grep "\.js" | uniq | sort > $domain/js-files.txt
+echo -e "[- Task Done -] \e[92mJs files of $domain are saved in js-files.txt\e[0m"
+
+echo "	"
+echo -e "\e[95m~ All files are saved in $domain directory\e[0m"
+echo -e "\e[5m	 Shubham Rooter:)\e[0m"
